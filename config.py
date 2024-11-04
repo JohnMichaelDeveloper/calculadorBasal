036fdf687f2f96c129103df019ba5a5cf36c9fed
JohnMichaelDeveloper <johnfrontend@gmail.com>
2024-11-04 00:31:28 -0300

    Atualizacao do config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'Itr451s#123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1@localhost:5432/calculationbasal'
+    SECRET_KEY = 'sua palavra chave'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://SeuUsuario:suaSenha@localhost:5432/calculationbasal'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
